1a4b89f76661f0e388f147e2a9a4149bd534d21b
tkofb <benard.kihiuria@gmail.com>
2023-07-25 18:20:07 -0400

    Removed password entity

--- push.sh
+++ push.sh
@@ -1,14 +1,14 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+source ~/scripts/push.sh
+
 git add .
 
 echo 'Enter the commit message:'
 read commitMessage
 
-echo 'Enter your ssh password:'
-read password
-
 git commit -m "$commitMessage"
 
-#work on this later
-echo password | git push
 
 
